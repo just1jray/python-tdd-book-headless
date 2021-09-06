1967ee731f779ab0cfb41c49ac43243373d0f120
Jesse McKnight <just1jray@gmail.com>
2021-09-05 20:23:54 -0700

    FT refactor

--- functional_tests.py
+++ functional_tests.py
@@ -13,6 +13,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Little Timmy has heard about a cool new online to-do app. He goes to check out its homepage
         self.browser.get('http://localhost:8000')
@@ -35,24 +40,19 @@ class NewVisitorTest(unittest.TestCase):
         # When he hits enter, the page updates, and now the page lists
         # "1: Buy tiny pink shirt" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(3)
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy tiny pink shirt', [row.text for row in rows])
+        time.sleep(1)
+        self.check_for_row_in_list_table('1: Buy tiny pink shirt')
         
         # There is still a text box inviting him to add another item.
         # He enters "Wear tiny pink shirt and listen to Wonton Soup"
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Wear tiny pink shirt and listen to Wonton Soup')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(3)
+        time.sleep(1)
         
         # The page updates again, and now shows both items on his list
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy tiny pink shirt', [row.text for row in rows])
-        self.assertIn('2: Wear tiny pink shirt and listen to Wonton Soup', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy tiny pink shirt')
+        self.check_for_row_in_list_table('2: Wear tiny pink shirt and listen to Wonton Soup')
 
         # Little Timmy wonders if the site will remember his list.
         # Then he sees that the site has generated a unique URL for him --
